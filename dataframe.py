05871129959e25ef094aae77180bfb41e893c51b
slim <=>
2017-10-04 22:28:32 +0200

    create and read from dataframe

--- dataframe.py
+++ dataframe.py
@@ -1,1 +1,11 @@
 import pandas as pd
+
+# read data 
+
+data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+        'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions', 'Lions', 'Lions'],
+        'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+        'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+football = pd.DataFrame(data, columns=['year', 'team', 'wins', 'losses'])
+print(football)
+
