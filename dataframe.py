b618439aa3d719362cf12fc1b9200039371027fa
slim <=>
2017-10-04 22:38:56 +0200

    read from csv source

--- dataframe.py
+++ dataframe.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 # read data 
-
+# use dict to build dataframe + specify columns
 data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
         'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions', 'Lions', 'Lions'],
         'wins': [11, 8, 10, 15, 11, 6, 10, 4],
         'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
 football = pd.DataFrame(data, columns=['year', 'team', 'wins', 'losses'])
 print(football)
+
+# read from csv
+from_csv = pd.read_csv('mariano-rivera.csv')
+from_csv.head()
 
